1f825bd07dd9286ca2dc6d064bb5347e12f5fbf7
Deepak Modi <deepakmodi8676@gmail.com>
2023-10-20 23:02:05 +0530

    added Multiply.java

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        int diff = a - b;
+        System.out.println("The difference is "+ diff);
+    }
+}
---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        int product = a * b;
+        System.out.println("The product is "+ product);
+    }
+}
