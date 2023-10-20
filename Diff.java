92166489a04fcf3e3e898fce2b191e74e150c1af
Deepak Modi <deepakmodi8676@gmail.com>
2023-10-20 23:26:15 +0530

    added implemenetaion for Diff.java

--- Diff.java
+++ Diff.java
@@ -1,8 +1,8 @@
 public class Diff {
     public static void main(String[] args) {
-        int a = 15;
-        int b = 10;
-        int diff = a - b;
+        int num1 = 20;
+        int num2 = 10;
+        int diff = num1 - num2;
         System.out.println("The difference is "+ diff);
     }
 }
